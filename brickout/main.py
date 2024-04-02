8f84b5ba3db106022632f205e0e0d4c643e0e31e
Abaranasia <rankirlian@gmail.com>
2024-04-02 09:41:23 +0200

    Fix: add paddle class

--- brickout/main.py
+++ brickout/main.py
@@ -2,16 +2,8 @@ import sys
 import pygame
 
 def main():
-    # Game setup
-    screen_width = 800
-    screen_height = 600
-    screen = pygame.display.set_mode((screen_width, screen_height))
-    pygame.display.set_caption('Brickout') # window title
 
-    clock = pygame.time.Clock()
-    background = (0, 0, 64)
-
-    # Ball class
+# Game items
     class Ball(pygame.sprite.Sprite):
         def __init__(self):
             pygame.sprite.Sprite.__init__(self)
@@ -31,12 +23,47 @@ def main():
                 self.speed[1] = -self.speed[1]
             elif self.rect.right >= screen_width or self.rect.left <= 0:
                 self.speed[0] = -self.speed[0]
+
             # Move
             self.rect.move_ip(self.speed)
 
+    class Paddle(pygame.sprite.Sprite):
+        def __init__(self):
+            pygame.sprite.Sprite.__init__(self)
+            self.image = pygame.image.load('brickout/assets/paddle.png')
+            self.rect = self.image.get_rect()
+
+            # Paddle initial position (screen center)
+            self.rect.midbottom = (screen_width / 2, screen_height -20)
+
+            # Paddle speed
+            self.speed= [0, 0]
+
+        def update(self, event):
+            if event.key == pygame.K_LEFT:
+                self.speed = [-5, 0]
+            elif event.key == pygame.K_RIGHT:
+                self.speed = [5, 0]
+            else:
+                self.speed = [0, 0]
+            
+            # Move
+            self.rect.move_ip(self.speed)
+
+# Game setup
+    screen_width = 800
+    screen_height = 600
+    background = (0, 0, 64)
+
+    screen = pygame.display.set_mode((screen_width, screen_height)) # screen size
+    pygame.display.set_caption('Brickout') # window title
+    clock = pygame.time.Clock() # game clock interval
+    pygame.key.set_repeat(10) # adjust key repetition on press down
 
     ball = Ball()
+    player = Paddle()
 
+# Game logic
     while True:
         # Set FPS
         clock.tick(60)
@@ -45,6 +72,8 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                player.update(event)
         
         # Update ball position
         ball.update()
@@ -52,6 +81,8 @@ def main():
         # Paint elements in screen
         screen.fill(background)
         screen.blit(ball.image, ball.rect)
+        screen.blit(player.image, player.rect)
+
         # Update screen items
         pygame.display.flip()
 
