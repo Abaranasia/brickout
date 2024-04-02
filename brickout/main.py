eeb740c2663dd931460fbd6d1f792eea3d62d96f
Abaranasia <rankirlian@gmail.com>
2024-04-02 19:10:26 +0200

    Chore: add game_over logic

--- brickout/main.py
+++ brickout/main.py
@@ -1,9 +1,15 @@
 import sys
+import time
 import pygame
 
 def main():
-
+    
 # Game items
+    screen_width = 800
+    screen_height = 600
+    background = (0, 0, 64)
+    
+    pygame.init()
     
 # Ball
     class Ball(pygame.sprite.Sprite):
@@ -21,7 +27,8 @@ def main():
 
         def update(self):
             # Boundaries collision detection
-            if self.rect.bottom >= screen_height or self.rect.top <= 0:
+            # if self.rect.bottom >= screen_height or self.rect.top <= 0:
+            if self.rect.top <= 0: # Check only top to allow player dead
                 self.speed[1] = -self.speed[1]
             elif self.rect.right >= screen_width or self.rect.left <= 0:
                 self.speed[0] = -self.speed[0]
@@ -43,6 +50,7 @@ def main():
             self.speed= [0, 0]
 
         def update(self, event):
+            # Player event handlers
             if event.key == pygame.K_LEFT and self.rect.left > 0:
                 self.speed = [-5, 0]
             elif event.key == pygame.K_RIGHT  and self.rect.right < screen_width:
@@ -79,12 +87,19 @@ def main():
                     pos_x = 0
                     pos_y += brick.rect.height
 
+# Game over logic
+    def game_over():
+        available_fonts= pygame.font.get_fonts()
+        message_font = pygame.font.SysFont(available_fonts[0], 72)
+        message = message_font.render('Game over :/ ', True, (255, 255, 255))
+        message_rect = message.get_rect()
+        message_rect.center = [screen_width / 2, screen_height / 2]
+        screen.blit(message, message_rect)
+        pygame.display.flip()
+        time.sleep(3)
+        sys.exit()
 
 # Game setup
-    screen_width = 800
-    screen_height = 600
-    background = (0, 0, 64)
-
     screen = pygame.display.set_mode((screen_width, screen_height)) # screen size
     pygame.display.set_caption('Brickout') # window title
     clock = pygame.time.Clock() # game clock interval
@@ -122,13 +137,18 @@ def main():
                 ball.speed[0] = - ball.speed[0]
             else: 
                 ball.speed[1] = - ball.speed[1]
-            wall.remove(collided_brick)      
+            wall.remove(collided_brick)  # Remove a sprite from a group 
+        
+        # Check if ball rebase bottom boudary 
+        if ball.rect.top > screen_height:
+            game_over()
 
         # Paint elements in screen
         screen.fill(background)
         screen.blit(ball.image, ball.rect)
         screen.blit(player.image, player.rect)
         wall.draw(screen)
+        
         # Update screen items
         pygame.display.flip()
 
