79edccfc3d57e24ccb9861f99c8cbe8f579fd0d7
Abaranasia <rankirlian@gmail.com>
2024-04-02 19:55:51 +0200

    Chore: add player live management and refine game over logic

--- brickout/main.py
+++ brickout/main.py
@@ -10,7 +10,9 @@ def main():
     background = (0, 0, 64)
     text_color = (255, 255, 255)
     player_score = 0
-    available_fonts= pygame.font.get_fonts()
+    player_lives = 3
+    waiting_player = True
+    available_fonts = pygame.font.get_fonts()
 
     pygame.init()
     
@@ -102,13 +104,22 @@ def main():
         sys.exit()
 
 # Player score logic
-    def display_score():
+    def display_player_score():
         score_font = pygame.font.SysFont(available_fonts[2], 20)
-        score = score_font.render(str(player_score).zfill(5), True, text_color)
+        score_text = "Score: " + str(player_score).zfill(5)
+        score = score_font.render(score_text, True, text_color)
         score_rect = score.get_rect()
         score_rect.topleft =  [0, 0]
         screen.blit(score, score_rect)
-        pygame.display.flip()
+
+# Player lives logic
+    def display_player_lives():
+        lives_font = pygame.font.SysFont(available_fonts[2], 20)
+        lives_text = "Lives: " + str(player_lives).zfill(2)
+        lives = lives_font.render(lives_text, True, text_color)
+        lives_rect = lives.get_rect()
+        lives_rect.topright =  [screen_width, 0]
+        screen.blit(lives, lives_rect)
 
 # Game setup
     screen = pygame.display.set_mode((screen_width, screen_height)) # screen size
@@ -124,17 +135,27 @@ def main():
 # Game logic
     while True:
         # Set FPS
-        clock.tick(60)
+        clock.tick(150)
 
         # Check events
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            # keypress events
             elif event.type == pygame.KEYDOWN:
                 player.update(event)
+                if waiting_player == True and event.key == pygame.K_SPACE:
+                    waiting_player = False
+                    if ball.rect.centerx < screen_width / 2:
+                        ball.speed = [3 ,-3]
+                    else:
+                        ball.speed = [-3 , -3]
         
         # Update ball position
-        ball.update()
+        if waiting_player == False: 
+            ball.update()
+        else: 
+            ball.rect.midbottom = player.rect.midtop
 
         # Collision management
         if pygame.sprite.collide_rect(ball, player): # ball vs player
@@ -153,17 +174,23 @@ def main():
         
         # Check if ball rebase bottom boudary 
         if ball.rect.top > screen_height:
-            game_over()
-
+            player_lives -= 1
+            waiting_player = True
+            
         # Paint elements in screen
         screen.fill(background)
         screen.blit(ball.image, ball.rect)
         screen.blit(player.image, player.rect)
         wall.draw(screen)
-        display_score()
-        
+
+        display_player_score()
+        display_player_lives()
+
         # Update screen items
         pygame.display.flip()
+        
+        if player_lives <= 0: 
+            game_over()
 
 
 if __name__ == '__main__':
