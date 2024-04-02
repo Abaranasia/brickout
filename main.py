3a1a3c2c7027a83d5dabb33d552ea41f4a13e92a
Abaranasia <rankirlian@gmail.com>
2024-04-02 02:38:52 +0200

    Chore: add ball collision detection with screen boundaries

--- main.py
+++ main.py
@@ -25,6 +25,11 @@ class Ball(pygame.sprite.Sprite):
         self.speed= [3, 3]
 
     def update(self):
+        # Boundaries collision detection
+        if self.rect.bottom >= screen_height or self.rect.top <= 0:
+            self.speed[1] = -self.speed[1]
+        elif self.rect.right >= screen_width or self.rect.left <= 0:
+            self.speed[0] = -self.speed[0]
         # Move
         self.rect.move_ip(self.speed)
 
