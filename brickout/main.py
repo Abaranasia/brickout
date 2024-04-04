284347250af35f7ffd81c40de876f27c78926473
Abaranasia <rankirlian@gmail.com>
2024-04-04 20:18:05 +0200

    Chore: add a new level to the game flow

--- brickout/main.py
+++ brickout/main.py
@@ -8,6 +8,7 @@ screen_height = 600
 background = (0, 0, 64)
 text_color = (255, 255, 255)
 initial_FPS = 125
+start_level = 1
 
 available_fonts = pygame.font.get_fonts()
 
@@ -24,13 +25,13 @@ class Director:
         pygame.display.set_caption(title) # window title
         self.screen = pygame.display.set_mode(res) # screen size
         self.clock = pygame.time.Clock() # game clock interval
+        self.current_level = start_level
         self.scene = None
         self.scenes = {}
 
     def execute(self, initial_scene, fps = initial_FPS):
         self.scene = self.scenes[initial_scene]
         playing = True
-        print("LLEGO AQUI")
 
         while playing:
             self.clock.tick(fps) # Set FPS
@@ -81,15 +82,21 @@ class Scene:
 
     def screen_switch(self, scene):
         self.next_scene = scene
+
+    def increase_level(self, current_level):
+        return str(current_level + 1)
         
 class Scene_level1(Scene):
     def __init__(self):
         Scene.__init__(self)
 
+        self.bricks_counter = 20
         # Game element instances
         self.ball = Ball()
         self.player = Paddle()
-        self.wall = Wall(120)
+        self.wall = Wall_easy(self.bricks_counter)
+        self.current_level = 1
+
 
         self.player_score = 0
         self.player_lives = 3
@@ -132,11 +139,19 @@ class Scene_level1(Scene):
                 self.ball.speed[1] = - self.ball.speed[1]
             self.wall.remove(collided_brick)  # Remove a sprite from a group
             self.player_score += 10
+            self.bricks_counter -= 1
+            print("bricks_counter", self.bricks_counter)
         
         # Check if ball rebase bottom boudary 
         if self.ball.rect.top > screen_height:
             self.player_lives -= 1
             self.waiting_player = True
+        
+        if self.bricks_counter <= 0:
+            self.screen_switch('level_complete')
+            time.sleep(2)
+            next_level= 'level' + self.increase_level(self.current_level)
+            self.screen_switch(next_level)
 
         if self.player_lives <= 0: 
             # game_over() 
@@ -147,30 +162,102 @@ class Scene_level1(Scene):
         # Paint elements in screen
         screen.fill(background)
 
-        self.display_player_score(screen)
-        self.display_player_lives(screen)
+        display_player_score(screen, self.player_score)
+        display_player_lives(screen, self.player_lives)
         
         screen.blit(self.ball.image, self.ball.rect)
         screen.blit(self.player.image, self.player.rect)
         self.wall.draw(screen)
 
-    # Player score logic
-    def display_player_score(self, screen):
-        score_font = pygame.font.SysFont(available_fonts[2], 20)
-        score_text = "Score: " + str(self.player_score).zfill(5)
-        score = score_font.render(score_text, True, text_color)
-        score_rect = score.get_rect()
-        score_rect.topleft =  [0, 0]
-        screen.blit(score, score_rect)
+class Scene_level2(Scene):
+    def __init__(self):
+        Scene.__init__(self)
 
-    # Player lives logic
-    def display_player_lives(self, screen):
-        lives_font = pygame.font.SysFont(available_fonts[2], 20)
-        lives_text = "Lives: " + str(self.player_lives).zfill(2)
-        lives = lives_font.render(lives_text, True, text_color)
-        lives_rect = lives.get_rect()
-        lives_rect.topright =  [screen_width, 0]
-        screen.blit(lives, lives_rect)
+        self.bricks_counter = 60
+        # Game element instances
+        self.ball = Ball()
+        self.player = Paddle()
+        self.wall = Wall_middle(self.bricks_counter)
+
+
+        self.player_score = 0
+        self.player_lives = 3
+        self.waiting_player = True
+
+        pygame.key.set_repeat(10) # adjust key repetition on press down
+    
+    def event_handlers(self, events):
+        # Check keypress events
+        for event in events: 
+            if event.type == pygame.KEYDOWN:
+                self.player.update(event)
+                if self.waiting_player == True and event.key == pygame.K_SPACE:
+                    self.waiting_player = False
+                    if self.ball.rect.centerx < screen_width / 2:
+                        self.ball.speed = [3 ,-3]
+                    else:
+                        self.ball.speed = [-3 , -3]
+
+    def update(self):
+    # Update screen items
+
+        # Update ball position
+        if self.waiting_player == False: 
+            self.ball.update()
+        else: 
+            self.ball.rect.midbottom = self.player.rect.midtop
+
+        # Collision management
+        if pygame.sprite.collide_rect(self.ball, self.player): # ball vs player
+            self.ball.speed[1] = - self.ball.speed[1]
+        
+        collided_brick_list= pygame.sprite.spritecollide(self.ball, self.wall, False)
+        if collided_brick_list: 
+            collided_brick = collided_brick_list[0]
+            cx = self.ball.rect.centerx
+            if cx < collided_brick.rect.left or cx > collided_brick.rect.right:
+                self.ball.speed[0] = - self.ball.speed[0]
+            else: 
+                self.ball.speed[1] = - self.ball.speed[1]
+            self.wall.remove(collided_brick)  # Remove a sprite from a group
+            self.player_score += 10
+            self.bricks_counter -= 1
+            print("bricks_counter", self.bricks_counter)
+        
+        # Check if ball rebase bottom boudary 
+        if self.ball.rect.top > screen_height:
+            self.player_lives -= 1
+            self.waiting_player = True
+        
+        if self.bricks_counter <= 0:
+            self.screen_switch('level_complete')
+
+        if self.player_lives <= 0: 
+            # game_over() 
+            # self.playing = False
+            self.screen_switch('game_over')
+    
+    def display(self, screen):
+        # Paint elements in screen
+        screen.fill(background)
+
+        display_player_score(screen, self.player_score)
+        display_player_lives(screen, self.player_lives)
+        
+        screen.blit(self.ball.image, self.ball.rect)
+        screen.blit(self.player.image, self.player.rect)
+        self.wall.draw(screen)
+
+class Scene_level_complete(Scene):
+    def update(self):
+        self.playing = False    
+
+    def display(self, screen):
+        self.message_font = pygame.font.SysFont(available_fonts[0], 72)
+        self.message = self.message_font.render('Level completed! ', True, text_color)
+        self.message_rect = self.message.get_rect()
+        self.message_rect.center = [screen_width / 2, screen_height / 2]
+        screen.blit(self.message, self.message_rect)
 
 class Scene_game_over(Scene):
     def update(self):
@@ -244,7 +331,22 @@ class Brick(pygame.sprite.Sprite):
         self.rect.topleft = position
 
 # Wall
-class Wall(pygame.sprite.Group):
+class Wall_easy(pygame.sprite.Group):
+    def __init__(self, brick_quantity ):
+        pygame.sprite.Group.__init__(self)
+
+        pos_x = 0
+        pos_y = 125
+        for i in range(brick_quantity):
+            brick = Brick((pos_x,pos_y))
+            self.add(brick)
+            pos_x += brick.rect.width
+
+            if pos_x >= screen_width:
+                pos_x = 0
+                pos_y += brick.rect.height
+
+class Wall_middle(pygame.sprite.Group):
     def __init__(self, brick_quantity ):
         pygame.sprite.Group.__init__(self)
 
@@ -259,13 +361,23 @@ class Wall(pygame.sprite.Group):
                 pos_x = 0
                 pos_y += brick.rect.height
 
-# Game over logic
-def game_over():
+# Player score logic
+def display_player_score(screen, player_score):
+    score_font = pygame.font.SysFont(available_fonts[2], 20)
+    score_text = "Score: " + str(player_score).zfill(5)
+    score = score_font.render(score_text, True, text_color)
+    score_rect = score.get_rect()
+    score_rect.topleft =  [0, 0]
+    screen.blit(score, score_rect)
 
-    pygame.display.flip()
-    time.sleep(3)
-    sys.exit()
-
-
+# Player lives logic
+def display_player_lives(screen, player_lives):
+    lives_font = pygame.font.SysFont(available_fonts[2], 20)
+    lives_text = "Lives: " + str(player_lives).zfill(2)
+    lives = lives_font.render(lives_text, True, text_color)
+    lives_rect = lives.get_rect()
+    lives_rect.topright =  [screen_width, 0]
+    screen.blit(lives, lives_rect)
+    
 if __name__ == '__main__':
     main()
