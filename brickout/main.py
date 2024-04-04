f47ffb4dd36e6c52fe59dbc6bc8770e8ca019ef7
Abaranasia <rankirlian@gmail.com>
2024-04-04 21:25:00 +0200

    Chore: update main file to consume extracted helper functions

--- brickout/main.py
+++ brickout/main.py
@@ -10,10 +10,8 @@ from brickout.objects.ball import Ball
 from brickout.objects.paddle import Paddle
 from brickout.objects.wall import Wall
 
-# Game items
-
-
-available_fonts = pygame.font.get_fonts()
+# Import helper functions
+from brickout.helpers.helpers import *
 
 def main():
     director = Director('BrickOut', (screen_width, screen_height))
@@ -273,23 +271,6 @@ class Scene_game_over(Scene):
         self.message_rect.center = [screen_width / 2, screen_height / 2]
         screen.blit(self.message, self.message_rect)
 
-# Player score logic
-def display_player_score(screen, player_score):
-    score_font = pygame.font.SysFont(available_fonts[2], 20)
-    score_text = "Score: " + str(player_score).zfill(5)
-    score = score_font.render(score_text, True, text_color)
-    score_rect = score.get_rect()
-    score_rect.topleft =  [0, 0]
-    screen.blit(score, score_rect)
-
-# Player lives logic
-def display_player_lives(screen, player_lives):
-    lives_font = pygame.font.SysFont(available_fonts[2], 20)
-    lives_text = "Lives: " + str(player_lives).zfill(2)
-    lives = lives_font.render(lives_text, True, text_color)
-    lives_rect = lives.get_rect()
-    lives_rect.topright =  [screen_width, 0]
-    screen.blit(lives, lives_rect)
     
 if __name__ == '__main__':
     main()
